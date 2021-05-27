b0f203c1d5e6a95998c6881cac862589fdf16d33
Ajay <aajay693@gmail.com>
2021-05-27 16:50:38 +0530

    home

--- src/pages/login/firstpage.js
+++ src/pages/login/firstpage.js
@@ -4,7 +4,7 @@ import './App.css';
 import h from'./h'
 import {BrowserRouter as HashRouter , Route , Link , Switch , NavLink} from "react-router-dom";
 
-import { Navbar } from 'react-bootstrap';
+import { Navbar,Nav,NavDropdown,Form,FormControl } from 'react-bootstrap';
 import { Button,ButtonGroup } from 'react-bootstrap';
 import Login from './login';
 import SignUp from './signup';
@@ -33,31 +33,48 @@ class firstpage extends Component {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
       
-      <div>
      
-
-      </div>
-      
       <HashRouter>
-<div class="nav w-100">
-<div class=" container-fluid row">
-<div class="col">
-<img src={logo} class="logo"></img>
+       
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#"><img src={logo} class="logo"></img>
+</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll" className="justify-content-start">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' , maxWidth:"100%" }}
+      navbarScroll
+    >
+      <Nav.Link className="m-auto" href=""><Link class="tx" to="">Home</Link>
+</Nav.Link>
+<Nav.Link><Link class="tx" to="/t">Test
+</Link></Nav.Link>
 
-<Link class="navtext" to="">HOME
+      <Nav.Link   >Videos</Nav.Link>
+      <NavDropdown title="Courses"   id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#action3">Programs</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Maths</NavDropdown.Item>
+        
+        <NavDropdown.Item href="#action5">Science</NavDropdown.Item>
+      </NavDropdown>
+    
+    </Nav>
+    </Navbar.Collapse>
+    <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+
+    <Link class="" to="/l">
+    <Button variant="flat" style={{ backgroundColor: "rgb(58, 10, 170)", color: "white" ,marginLeft:"10px"}}>Login</Button>
+<Link class="navtext" to="/s">SignUp</Link>
+
+
 </Link>
-<Link class="navtext" to="/t">TESTS
-</Link>
-</div>
+  </Navbar.Collapse>
+</Navbar>
 
-<Link class=" col-1  align-self-end" to="/l">
-<button class="btn btn-primary"  > Login</button>
 
-</Link>
-<Link class="col-1  align-self-end navtext" to="/s">SignUp</Link>
-</div>
 
-</div>
+
      
           <Switch>
     <Route  exact={true} path='/' component={h}/>
@@ -66,11 +83,12 @@ class firstpage extends Component {
     <Route  exact={true} path='/t' component={test_d}/>
       <Route   exact={true} path='/s' component={SignUp}/>
     </Switch>
-    
+   
   </HashRouter>
     
-    
+  
     </div>
+  
 
     );
   }
--- src/pages/login/h.js
+++ src/pages/login/h.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from '../../logo.jpeg'
-import logo1 from '../../nn.png'
+import logo1 from '../../nnn.png'
+import { Button ,Table } from 'react-bootstrap';
 
 
 class h extends Component{
@@ -13,110 +14,151 @@ class h extends Component{
             <div id="home">
                 <br/>
                 <div class="container homebg">
-                    <div class="container-fluid ">
+                    <div class="container ">
                         <div class="row">
-                        <div class="col quote">
-                                "Never Stop Learning
+                        <div class="col ">
+                            <br/><br/>                            <br/><br/>
+
+                            <div class="quote">
+                            "Never Stop Learning
                                 Because
                                 Life Never Stop Teaching..."
                             </div>
-                            <div class="col">
+                               <br/><br/>
+                            <Button variant="flat" style={{ backgroundColor: "rgb(58, 10, 170)", color: "white" ,marginLeft:"10px"}}>Get in Touch</Button>
+                            </div>
+                            <div class="col ">
                             <img src={logo1} class="nn"></img>
 
                             </div>
                             
                         </div>
 
-
+                        </div>
 
                     </div>
                     <br/>
                     <div class="container-fluid w-100 quotebg">                  
-                    <h2 class="cc">COURSES</h2>
+                    <h4 class="cc">COURSES WE OFFER</h4>
                     <br/><br/>
-                    <div class="container row justify-content-around">
-                        <div clas="col-4">
+                    <div class="container">
+                        <div>
                             
                     <div class="row">
-                        <div class="col">
+                        <div class="col-sm">
                         <img src={logo} class="course-logo"></img>
-                            <h4 class="ch">ENGLISH</h4>
+                            <h6 class="ch">ENGLISH</h6>
                             <p>It helps people become better citizens, get a better-paid job, shows the difference between good and bad. Education shows us the importance of hard work and, at the same time, helps us grow and develop.</p>
                         <a href="#" class="ch">Read More.....</a>
                         </div>
-                        <div class="col">
+                        <div class="col-sm">
                         <img src={logo} class="course-logo"></img>
-                            <h4 class="ch">TAMIL</h4>
+                            <h6 class="ch">TAMIL</h6>
                             <p>It helps people become better citizens, get a better-paid job, shows the difference between good and bad. Education shows us the importance of hard work and, at the same time, helps us grow and develop.</p>
                             <a href="#" class="ch">Read More.....</a>
 
                         </div>
-                        <div class="col">
+                        <div class="col-sm">
                         <img src={logo} class="course-logo"></img>
-                            <h4 class="ch">MATHS</h4>
+                            <h6 class="ch">MATHS</h6>
                             <p>It helps people become better citizens, get a better-paid job, shows the difference between good and bad. Education shows us the importance of hard work and, at the same time, helps us grow and develop.</p>
                             <a href="#" class="ch">Read More.....</a>
 
                         </div>
-                    </div>
-                    </div>
                     </div>
                     <br/><br/><br/>
                     <div class="row">
-                        <div class="col">
+                        <div class="col-sm">
                         <img src={logo} class="course-logo"></img>
-                            <h4 class="ch">SCIENCE</h4>
+                            <h6 class="ch">SCIENCE</h6>
                             <p>It helps people become better citizens, get a better-paid job, shows the difference between good and bad. Education shows us the importance of hard work and, at the same time, helps us grow and develop.</p>
                             <a href="#" class="ch">Read More.....</a>
 
                         </div>
-                        <div class="col">
+                        <div class="col-sm">
                         <img src={logo} class="course-logo"></img>
-                            <h4 class="ch">PROGRAMS</h4>
+                            <h6 class="ch">PROGRAMS</h6>
                             <p>It helps people become better citizens, get a better-paid job, shows the difference between good and bad. Education shows us the importance of hard work and, at the same time, helps us grow and develop.</p>
                             <a href="#" class="ch">Read More.....</a>
 
                         </div>
-                        <div class="col">
+                        <div class="col-sm">
                         <img src={logo} class="course-logo"></img>
-                            <h4 class="ch">COMPUTER SCIENCE</h4>
+                            <h6 class="ch">COMPUTER SCIENCE</h6>
                             <p>It helps people become better citizens, get a better-paid job, shows the difference between good and bad. Education shows us the importance of hard work and, at the same time, helps us grow and develop.</p>
                             <a href="#" class="ch">Read More.....</a>
 
                         </div>
                     </div>
                     </div>
-                </div>
+                    </div>
+                   
+                    </div>
+               
                 <br/>
                 <br/>
-                <div class="container-fluid topbg">
-                    <h4><marquee>Topachievers section <br/>Under Construction</marquee></h4>
+                <div class="container topbg">
+                    <h4 class="cc">TOP ACHIEVERS</h4>
+                    <br/><br/>
+                <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>S No</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Score</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>100</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>98</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>97</td>
+    </tr>
+  </tbody>
+</Table>
                 </div>
                 <br/>                <br/>
                 <br/>
                 
 
-                <div class="container-fluid row">
-                    <div class="col">
+                <div class="container">
+                <h4 class="cc">FEATURED VIDEOS</h4>
+                    <br/><br/>
+                    <div class="row">
+                    <div class="col-sm">
                     <img src={logo} class="course-logo"></img>
-
+<br/><br/>
                         video section
                     </div>
-                    <div class="col">
+                    <div class="col-sm">
                     <img src={logo} class="course-logo"></img>
-
+                    <br/><br/>
                         video section
                     </div>
-                    <div class="col">
+                    <div class="col-sm">
                     <img src={logo} class="course-logo"></img>
-
+                    <br/><br/>
                         video section
                     </div>
-                    <div class="col">
+                    <div class="col-sm">
                     <img src={logo} class="course-logo"></img>
-
+                    <br/><br/>
                         video section
                     </div>
+                  </div>
              </div>
              <br/>
              <br/>
@@ -126,7 +168,7 @@ class h extends Component{
 
              <div class="container-fluid footer">
                 <div class="row">
-                <div class="col">
+                <div class="col ch">
                     
                     Privacy and policy
                 </div>
